63ad2b3195847dd859cda6db2f4c62926a0e2237
mihili <mihilisanjula@gmail.com>
2025-04-26 21:58:59 +0530

    Revert "Merge pull request #41 from Mihiliupendra/post"
    This reverts commit 72279bea4dd89a12f0f3af01faed00df355ce41a, reversing
    changes made to d77808f1af370f0b876aac397c5cc4512112b656.

--- backend/demo/src/main/java/com/example/demo/model/MediaType.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model;
-
-public enum MediaType {
-    IMAGE,
-    VIDEO;
-
-}
--- backend/demo/src/main/java/com/example/demo/model/PostType.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public enum PostType {
-    SKILL_SHARING,
-    LEARNING_PROGRESS,
-    LEARNING_PLAN;
-
-}
